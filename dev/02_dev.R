0519f44b332209d0cfbfb93c3ad03569b028d4fa
JustusWeyers <justus.weyers@posteo.de>
2024-02-01 18:11:09 +0100

    update namespace

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -16,17 +16,18 @@
 ## Dependencies ----
 ## Amend DESCRIPTION with dependencies read from package code parsing
 ## install.packages('attachment') # if needed.
+attachment::att_from_rscripts() |>
+  purrr::map(usethis::use_package)
 attachment::att_amend_desc()
 
 ## Add modules ----
 ## Create a module infrastructure in R/
-golem::add_module(name = "name_of_module1", with_test = TRUE) # Name of the module
-golem::add_module(name = "name_of_module2", with_test = TRUE) # Name of the module
+golem::add_module(name = "database" ) # Name of the module
+golem::add_module(name = "ts_upload" ) # Name of the module
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
-golem::add_fct("helpers", with_test = TRUE)
-golem::add_utils("helpers", with_test = TRUE)
+golem::add_utils("connect_database")
 
 ## External resources
 ## Creates .js and .css files at inst/app/www
